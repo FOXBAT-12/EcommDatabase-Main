205de48b8f47130c289a4e84cce3a3d77e098388
Prakash10k <Prakash.Kurram@cognizant.com>
2025-04-10 09:59:11 +0530

    Update Category.cs

--- Ecomm-Database-Class/Ecomm-Database-Class/Model/Category.cs
+++ Ecomm-Database-Class/Ecomm-Database-Class/Model/Category.cs
@@ -10,7 +10,7 @@ namespace Ecomm_Database_Class.Model
     public class Category
     {
         [Key]
-        public int Id { get; set; }
+        public int CategoryId { get; set; }
         [Required(ErrorMessage = "Name is required")]
         [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters")]
         public string Name { get; set; } = string.Empty;
