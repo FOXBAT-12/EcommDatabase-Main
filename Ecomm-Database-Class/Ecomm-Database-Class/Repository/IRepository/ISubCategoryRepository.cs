c35a47f5d4b1969c60d34a1e1e3812cd25b1422a
Prakash10k <Prakash.Kurram@cognizant.com>
2025-04-10 09:31:10 +0530

    Update ISubCategoryRepository.cs

--- Ecomm-Database-Class/Ecomm-Database-Class/Repository/IRepository/ISubCategoryRepository.cs
+++ Ecomm-Database-Class/Ecomm-Database-Class/Repository/IRepository/ISubCategoryRepository.cs
@@ -1,4 +1,4 @@
-﻿using Ecomm_Database_Class.Model;
+using Ecomm_Database_Class.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +10,7 @@ namespace Ecomm_Database_Class.Repository.IRepository
     public interface ISubCategoryRepository
     {
         Task<IEnumerable<SubCategory>> GetAllAsync();
-        Task<SubCategory?> GetByIdAsync(int id);
+        Task<SubCategory?> GetAllAsync(int id);
         Task<SubCategory> AddAsync(SubCategory subCategory);
         Task<SubCategory?> UpdateAsync(SubCategory subCategory);
         Task<bool> DeleteAsync(int id);
