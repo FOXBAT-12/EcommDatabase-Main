fe6c06fe55afe8964e9688feab61cf4a48465c23
Prakash10k <Prakash.Kurram@cognizant.com>
2025-04-10 09:58:14 +0530

    Update SubCategory.cs

--- Ecomm-Database-Class/Ecomm-Database-Class/Model/SubCategory.cs
+++ Ecomm-Database-Class/Ecomm-Database-Class/Model/SubCategory.cs
@@ -9,17 +9,14 @@ using System.Threading.Tasks;
 namespace Ecomm_Database_Class.Model
 {
     public class SubCategory
-{
-    [Key]
-    public int Id { get; set; }
-    [Required(ErrorMessage = "Name is required")]
-    [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters")]
-    public string Name { get; set; } = string.Empty;
-    [Required(ErrorMessage = "CategoryId is required")]
-    public int CategoryId { get; set; }
-    [ForeignKey("CategoryId")]
-    public Category? Category { get; set; }
-
-    public ICollection<Product>? Products { get; set; }
-}
+    {
+        [Key]
+        public int SubCategoryId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name can't be longer than 100 characters")]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "CategoryId is required")]
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+    }
 }
